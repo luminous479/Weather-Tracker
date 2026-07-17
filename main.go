dd3e944e3ff4b8e2fe30e95afdfb4c5a3fe7446b
luminous479 <aklemasultanakhi@gmail.com>
2026-07-17 23:57:45 +0600

    feat(config): add API configuration loader and weather response models

--- main.go
+++ main.go
@@ -1,1 +1,80 @@
 package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"strings"
+)
+
+type apiConfigData struct {
+	APIKey string `json:"OpenWeatherMapApiKey"`
+}
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func loadAPIConfig(filename string) (*apiConfigData, error) {
+	configFile, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer configFile.Close()
+
+	var config apiConfigData
+	err = json.NewDecoder(configFile).Decode(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+
+func query(city string) (*weatherData, error) {
+	config, err := loadAPIConfig(".apiConfig")
+	if err != nil {
+		return nil, err
+	}
+
+	apiKey := config.APIKey
+	url := "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var data weatherData
+	err = json.NewDecoder(resp.Body).Decode(&data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
+
+func main() {
+
+
+	http.HandleFunc("/hello",hello)
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+     city := strings.SplitN(r.URL.Path,"/",3)[2]
+	 data, err := query(city)
+	 if err != nil {
+		 http.Error(w, err.Error(), http.StatusInternalServerError)
+		 return
+	 }
+	 w.Header().Set("Content-Type", "application/json")
+	 json.NewEncoder(w).Encode(data)
+	})
+	http.ListenAndServe(":8080", nil)
+}
